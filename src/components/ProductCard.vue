ced56090029c6a4f94f32fe7f12e0eb5c6933673
Erick Marcelo <marcerican@gmail.com>
2022-02-17 11:53:30 -0500

    Create product card component

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <div v-if="product" class="card">
+    <div class="card__img" @click="toggleInfo">
+      <img :src="product.photo" alt="card image" />
+      <div v-if="product.stock > 0" class="stock">Disponible</div>
+      <div v-else class="stock">Sin Stock</div>
+    </div>
+    <div class="info">
+      <div class="card__information">
+        <h3 class="card__info">{{ shortName }}</h3>
+        <p class="card__info">Precio: S/.{{ product.price }}</p>
+      </div>
+      <div class="card__action"></div>
+    </div>
+  </div>
+  <div v-else>Loading</div>
+</template>
+
+<script>
+import { computed } from "vue";
+
+export default {
+  props: ["product"],
+  setup(props) {
+    const shortName = computed(() => {
+      if (props.product.name.length >= 30)
+        return props.product.name.slice(0, 30) + " ...";
+      return props.product.name;
+    });
+
+    return { shortName };
+  },
+};
+</script>
+
+<style scoped>
+@import url("../styles/ProductCard.css");
+</style>
--- src/views/Category.vue
+++ src/views/Category.vue
@@ -4,20 +4,23 @@
     <h2>Home > Products > {{ name }}</h2>
     <p v-if="error">Something went wrong ...</p>
     <div v-if="!loading" class="products">
-      <div v-for="product in matchCategory" :key="product.id">
-        <p>{{ product.name }}</p>
-      </div>
+      <ProductCard
+        v-for="product in matchCategory"
+        :key="product.id"
+        :product="product"
+      />
     </div>
     <div v-else>Loading</div>
   </div>
 </template>
 
 <script>
+import ProductCard from "../components/ProductCard.vue";
 import getProducts from "../composables/getProducts";
 
 export default {
   name: "Category",
-  components: {},
+  components: { ProductCard },
   props: ["name"],
   setup(props) {
     const { products, loading, error, fetchProducts, matchCategory } =
