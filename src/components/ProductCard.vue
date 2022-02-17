26685af1da15d3e16b24b1e442642d8e406c5c3a
Erick Marcelo <marcerican@gmail.com>
2022-02-17 12:02:14 -0500

    Create product details component

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -3,32 +3,39 @@
     <div class="card__img" @click="toggleInfo">
       <img :src="product.photo" alt="card image" />
       <div v-if="product.stock > 0" class="stock">Disponible</div>
-      <div v-else class="stock">Sin Stock</div>
     </div>
     <div class="info">
       <div class="card__information">
         <h3 class="card__info">{{ shortName }}</h3>
         <p class="card__info">Precio: S/.{{ product.price }}</p>
       </div>
+
       <div class="card__action"></div>
     </div>
+    <ProductDetails v-if="info" :product="product" :toggle="toggleInfo" />
   </div>
   <div v-else>Loading</div>
 </template>
 
 <script>
-import { computed } from "vue";
+import { computed, ref } from "vue";
+import ProductDetails from "./ProductDetails.vue";
 
 export default {
   props: ["product"],
+  components: { ProductDetails },
   setup(props) {
+    const info = ref(false);
+    const toggleInfo = () => {
+      info.value = !info.value;
+    };
     const shortName = computed(() => {
       if (props.product.name.length >= 30)
         return props.product.name.slice(0, 30) + " ...";
       return props.product.name;
     });
 
-    return { shortName };
+    return { shortName, toggleInfo, info };
   },
 };
 </script>
---
+++ src/components/ProductDetails.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="backdrop">
+    <div v-if="product" class="product">
+      <div class="header">
+        <h2>Producto Agregado</h2>
+        <p @click="toggle">X</p>
+      </div>
+      <div class="information">
+        <div class="information__principal">
+          <img :src="product.photo" alt="" />
+          <div>
+            <p>Name : {{ product.name }}</p>
+            <p>Code : {{ product.code }}</p>
+            <p>Stock : {{ product.stock }}</p>
+            <p>Price : S/. {{ product.price }}</p>
+          </div>
+        </div>
+        <div class="information__secondary">
+          <p>
+            Description: <span>{{ product.description }}</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["product", "toggle"],
+  setup() {},
+};
+</script>
+
+<style scoped>
+@import url("../styles/ProductDetails.css");
+</style>
